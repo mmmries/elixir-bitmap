2a11a288280793d06adeb395d177fa31a0b1e130
hqmq <michael@riesd.com>
2015-04-12 07:33:40 -0600

    reduce without a nested reduce

--- lib/enumerable/bitmap.ex
+++ lib/enumerable/bitmap.ex
@@ -18,16 +18,16 @@ defimpl Enumerable, for: Bitmap do
   def reduce(_bitmap, {:halt, acc}, _reducer), do: {:halted, acc}
   def reduce(bitmap, {:suspend, acc}, reducer), do: {:suspended, acc, &reduce(bitmap, &1, reducer)}
   def reduce(%Bitmap{chunks: chunks}, acc, reducer) do
-    {:cont, acc} = chunks |> Enum.reduce acc, fn({address, chunk}, acc) ->
-      reduce({address,chunk,0}, acc, reducer)
-    end
-    {:done, acc}
+    chunks = Map.to_list(chunks)
+    reduce({chunks}, acc, reducer)
   end
-  def reduce({address, 0, _which_bit}, {:cont, acc}, _reducer), do: {:cont, acc}
-  def reduce({address, chunk, which_bit}, {:cont, acc}, reducer) do
+  def reduce({[]}, {:cont, acc}, _reducer), do: {:done, acc}
+  def reduce({[h|t]}, {:cont, acc}, reducer), do: reduce({t, h, 0}, {:cont, acc}, reducer)
+  def reduce({chunks,{_address,0},_which_bit}, acc, reducer), do: reduce({chunks}, acc, reducer)
+  def reduce({chunks,{address,chunk}, which_bit}, {:cont, acc}, reducer) do
     case chunk &&& 1 do
-      1 -> reduce({address,chunk>>>1,which_bit+1}, reducer.(Bitmap.to_number(address,which_bit),acc), reducer)
-      0 -> reduce({address,chunk>>>1,which_bit+1}, {:cont, acc}, reducer)
+      1 -> reduce({chunks, {address,chunk>>>1}, which_bit+1}, reducer.(Bitmap.to_number(address,which_bit),acc), reducer)
+      0 -> reduce({chunks, {address,chunk>>>1}, which_bit+1}, {:cont, acc}, reducer)
     end
   end
 end
