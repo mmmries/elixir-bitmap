96944a21f1bf6cbd94bc9665e2394f2852728abe
hqmq <michael@riesd.com>
2015-04-23 19:21:18 -0600

    use a list + Enum.sort as a benchmark for performance

--- lib/tasks/sort_file.ex
+++ lib/tasks/sort_file.ex
@@ -3,20 +3,20 @@ defmodule Mix.Tasks.SortFile do
   @shortdoc "reads in a file of randomized numbers and outputs a sorted list"
 
   def run(_) do
-    bitmap = read_numbers(Bitmap.create, :stdio)
-    Enum.each(bitmap, fn (number) ->
+    list = read_numbers([], :stdio)
+    list = Enum.sort(list)
+    Enum.each(list, fn (number) ->
       line = (number |> Integer.to_string) <> "\n"
       IO.binwrite(:stdio, line)
     end)
   end
 
-  defp read_numbers(bitmap, device) do
+  defp read_numbers(list, device) do
     case IO.read(device, :line) do
-      :eof -> bitmap
+      :eof -> list
       line ->
         number = line |> String.rstrip |> String.to_integer
-        bitmap = bitmap |> Bitmap.store(number)
-        read_numbers(bitmap, device)
+        read_numbers([number|list], device)
     end
   end
 end
